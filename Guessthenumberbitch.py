34e1b008cbf5402f93b3d5d0e3c7d011a39ad20b
bettersleepearly <66324148+bettersleepearly@users.noreply.github.com>
2020-06-02 15:24:10 -0400

    Update Guessthenumberbitch.py

--- Guessthenumberbitch.py
+++ Guessthenumberbitch.py
@@ -62,13 +62,19 @@ while guess != number:
         print("Fine fine! You win! The number was", number, "and it was only", tries, "tries!")
         print()
 
-    question = input("Wanna try again? [Y/N]")
+        
+        
+        
+question1 = input("Wanna try again? [Y/N]")
 
-    if question == "N":
-        print()
-        print("Fine fuck off then :)")
+if question1 == "N":
+    print()
+    print("Fine fuck off then :)")
 
     
-    if question == "Y":
-        print("let's fucking go then")
-        print()
+if question1 == "Y":
+    print("let's fucking go then")
+    print()
+    print("Now, on a scale from 1 to 10")
+    print("How much of an asshole u think u are? :)")
+    guess = int(input("guess quick:"))
